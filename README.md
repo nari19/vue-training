83034122c10f3af1d0e2d722660191a9b982e3dd
nari19 <kazuki7793996@gmail.com>
2021-04-11 08:50:41 +0900

    form html

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - [作りながら学ぶ Vue.js パート 2](https://note.com/tis_engineer/n/ne6ff2e82196b)
 
 - [作りながら学ぶ Vue.js パート 3](https://note.com/tis_engineer/n/n38cba81f0feb)
+
+- [作りながら学ぶ Vue.js パート 4](https://note.com/tis_engineer/n/n3bae5ea84c14)
---
+++ src/components/Form.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="item">
+      <input type="text" name="title" />
+    </div>
+    <div class="item">
+      <input type="text" name="period" />
+    </div>
+    <div class="item">
+      <textarea name="detail"></textarea>
+    </div>
+    <div class="item">
+      <button>作成</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
