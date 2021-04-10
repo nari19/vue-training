5b917c03e9da64ddc544316e99a8efe4a6233b7a
nari19 <kazuki7793996@gmail.com>
2021-04-11 08:32:28 +0900

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="app">タスク管理</div>
+  <div id="app">
+    <Header />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="header">
+    <h1>タスク管理アプリケーション</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+}
+</style>
